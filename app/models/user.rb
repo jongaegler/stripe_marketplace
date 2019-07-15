3ddc93a4e7ccdd2dcbe6279128488172839298a5
Jon Gaegler <jgaegler@gmail.com>
2019-07-14 22:25:23 -0400

    Move the initialer to the devise file rather than it's own file

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :omniauthable, :omniauth_providers => [:stripe_connect],
-         :registerable, :recoverable, :rememberable, :validatable
+  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: [:stripe_connect]
 end
--- config/initializers/omniauth.rb
+++
@@ -1,3 +0,0 @@
-Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :stripe_connect, Rails.application.credentials.stripe[:client_id], Rails.application.credentials.stripe[:secret_key]
-end
