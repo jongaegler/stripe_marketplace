72894e5035617325e726fa2d29501102dc3b98ee
Jon Gaegler <jgaegler@gmail.com>
2019-07-17 00:42:23 -0400

    Read devise docs to learn about omniauth

--- app/controllers/omniauth_callbacks_controller.rb
+++ app/controllers/omniauth_callbacks_controller.rb
@@ -1,8 +1,15 @@
 class OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
   def stripe_connect
-    # Delete the code inside of this method and write your own.
-    # The code below is to show you where to access the data.
-    raise request.env['omniauth.auth'].to_yaml
+    user = User.from_stripe(request.env['omniauth.auth'])
+
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activated
+      set_flash_message(:notice, :success, kind: "Facebook") if is_navigational_format?
+    else
+      session["devise.facebook_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+    redirect_to '/'
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,17 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
          :omniauthable, omniauth_providers: [:stripe_connect]
+
+  def self.from_stripe(params)
+    user = User.find_or_create_by(uid: params['uid'])
+    return user unless user.persisted?
+    user.update(
+
+      email: params['info']['email']
+    )
+  end
+
+  def failure
+    redirect_to root_path
+  end
 end
