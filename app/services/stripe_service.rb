b291449e04cc048a7fe043ab2d89efbd34b93c2d
Jon Gaegler <jgaegler@gmail.com>
2019-07-18 01:04:28 -0400

    Add example serverside checkout logic

--- app/services/stripe_service.rb
+++ app/services/stripe_service.rb
@@ -1,5 +1,14 @@
 class StripeService
 
+  def checkout(product)
+    Stripe::Checkout::Session.create(
+      success_url: 'https://example.com/success',
+      payment_method_types: ['card'],
+      line_items: product,
+      cancel_url: 'https://example.com/cancel'
+    )
+  end
+
   def charge(price, account_id)
     price = product.price
     Stripe::Charge.create(
